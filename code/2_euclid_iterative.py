82a2381f174bec8f643be29f76837361455fd438
0xDFDFDF <757111+0xDFDFDF@users.noreply.github.com>
2019-10-07 21:59:35 +0200

    poprawki

--- code/2_euclid_iterative.py
+++ code/2_euclid_iterative.py
@@ -1,7 +1,11 @@
 def euclid(a, b):
   r = a % b
+  print('{}=q*{}+{}'.format(a, b, r))
   while r != 0:
     a = b
     b = r
     r = a % b
+    print('{}=q*{}+{}'.format(a, b, r))
   return b
+
+euclid(1071, 462)
--- code/2_factorial_iterative.py
+++ code/2_factorial_iterative.py
@@ -1,6 +1,22 @@
-def iterative_factorial(n):        
-    x = 1
-    li = list(range(2, n+1))
-    for each in li:
-        x = x*each
-        yield x
+def fct(n):
+  k = n
+  result = k
+  while k > 1:
+    k = k - 1
+    result = result * k
+  return result
+
+fct(11)
+
+### Wprowadźmy pętle FOR
+for k in range (1, 5):
+  print(k)
+
+###
+def fct2(n):
+  result = n
+  for k in range(1, n):
+    result = result * k
+  return result
+
+fct2(5)
